0072b57c457f107096f9092228c78b368b64516b
Behera Manav <manavbehera123@gmail.com>
2024-08-26 22:59:42 +0530

    Dynamic Data insertion

--- src/component/App.jsx
+++ src/component/App.jsx
@@ -2,13 +2,19 @@ import Logo from "./Logo";
 import Form from "./Form";
 import PackingList from "./PackingList";
 import Stats from "./Stats";
+import { useState } from "react";
 
 function App() {
+  const [items, setItem] = useState([]);
+
+  function handleAddItem(item) {
+    setItem((items) => [...items, item]);
+  }
   return (
     <div className="app">
       <Logo />
-      <Form />
-      <PackingList />
+      <Form onAddItems={handleAddItem} />
+      <PackingList list={items} />
       <Stats />
     </div>
   );
--- src/component/Form.jsx
+++ src/component/Form.jsx
@@ -1,5 +1,37 @@
-export default function Form() {
-  return <div className="add-form">
-    <h3>what do you need for your 😍 trip ?</h3>
-  </div>
+import { useState } from "react";
+
+export default function Form({ onAddItems }) {
+  const [description, setdescription] = useState("");
+  const [quantity, setquantity] = useState(1);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (!description) return;
+    const newItem = { description, quantity, packed: false, id: Date.now() };
+    onAddItems(newItem);
+    setdescription("");
+    setquantity(1);
+  }
+
+  return (
+    <form className="add-form" onSubmit={handleSubmit}>
+      <h3>what do you need for your 😍 trip ?</h3>
+      <select
+        value={quantity}
+        onChange={(e) => setquantity(Number(e.target.value))}
+      >
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+          <option value={num} key={num}>
+            {num}
+          </option>
+        ))}
+      </select>
+      <input
+        type="text"
+        placeholder="Item..."
+        onChange={(e) => setdescription(e.target.value)}
+      ></input>
+      <button>Add</button>
+    </form>
+  );
 }
