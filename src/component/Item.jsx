a6414d12bdc0fdb96503c15c9a684cef1a33beaa
Behera Manav <manavbehera123@gmail.com>
2024-08-28 22:45:06 +0530

    feat/delete functionality of item

--- src/component/Item.jsx
+++ src/component/Item.jsx
@@ -1,10 +1,10 @@
-export default function Item({ item }) {
+export default function Item({ item, onDeleteItem }) {
   return (
     <li>
       <span style={item.packed ? { textDecoration: "line-through" } : {}}>
         {item.quantity} {item.description}
       </span>
-      <button>❌</button>
+      <button onClick={() => onDeleteItem(item.id)}>❌</button>
     </li>
   );
 }
